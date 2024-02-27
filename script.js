b5ce5b6e573180781fa9a7e5730489ccb6cf7384
Aadhi1111 <94366686+Aadhi1111@users.noreply.github.com>
2024-02-27 23:25:05 +0530

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-function f1(){
-    console.log("vera maari");
-    function f2(){
-        console.log("new maari");
-        function f3(){
-            console.log("maari");
-            function f4(){
-                console.log("Hello World");
-            }
-        }
-    }
-}
+var heading = document.createElement("h1");
+heading.setAttribute("class","text-white text-center");
 
-f1();
+
+document.body.append(heading);
+
+setTimeout(function(){
+    document.querySelector('h1').innerHTML = `Counter: ${10}`;
+    setTimeout(function(){
+        document.querySelector("h1").innerText = `Counter: ${9}`;
+        setTimeout(function(){
+            document.querySelector("h1").innerText = `Counter: ${8}`;
+            setTimeout(function(){
+                document.querySelector("h1").innerText = `Counter: ${7}`;
+                setTimeout(function(){
+                    document.querySelector("h1").innerText = `Counter: ${6}`;
+                    setTimeout(function(){
+                        document.querySelector("h1").innerText = `Counter: ${5}`;
+                        setTimeout(function(){
+                            document.querySelector("h1").innerText = `Counter: ${4}`;
+                            setTimeout(function(){
+                                document.querySelector("h1").innerText = `Counter: ${3}`;
+                                setTimeout(function(){
+                                    document.querySelector("h1").innerText = `Counter: ${2}`;
+                                    setTimeout(function(){
+                                        document.querySelector("h1").innerText = `Counter: ${1}`;
+                                        setTimeout(function(){
+                                            document.querySelector("h1").innerText = `HAPPY INDEPENDENCE DAY`;
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000)
+},1000);
